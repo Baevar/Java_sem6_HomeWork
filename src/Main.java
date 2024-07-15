59895cfa72da3c24fff07741c668f8f42a39f3c6
Oleg Dobrovolskikh <baevar@mail.ru>
2024-07-15 23:01:04 +0700

    create new method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 //Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
 //
@@ -23,13 +21,47 @@ public class Main {
         int countLaptop = 20; //количество ноутбуков
         Set<Laptops> laptops = new HashSet<>();
         createSetLaptop(laptops, countLaptop);
-        for (Laptops laptop : laptops) {
-            System.out.println(laptop);
+
+
+        Scanner scan = new Scanner(System.in);
+        boolean flag = true;
+        System.out.println("Здравствуйте! ");
+        while (flag) {
+            System.out.println("Пожалуйста, введите число команды для выполнения действия:");
+            System.out.println("1 - Показать список ноутбуков");
+            System.out.println("2 - Фильтрация списка ноутбуков");
+            System.out.println("3 - Закончить работу");
+            String action = scan.next();
+            if (isNumber(action)) {
+                switch (Integer.parseInt(action)) {
+                    case 1:
+                        for (Laptops laptop : laptops) {
+                            System.out.println(laptop);
+                        }
+                        System.out.println();
+                        break;
+                    case 2:
+                        filterLaptop(laptops);
+                        break;
+                    case 3:
+                        flag = !flag;
+                        System.out.println("Спасибо за использование системы! Всего хорошего!");
+                        break;
+                    default:
+                        System.out.println("Некорректный ввод!");
+                        break;
+                }
+            } else {
+                System.out.println("Ошибка ввода");
+            }
+
         }
+
 
     }
 
-//    Создаю список ноутбуков случайным образом по заведомо известным параметрам
+
+    // Создаю список ноутбуков случайным образом по заведомо известным параметрам
     public static void createSetLaptop(Set laptops, int countLaptop) {
 
         String[] rndModel = {"MSI", "Acer", "Huawei", "Asus", "Macbook", "Lenovo"};
@@ -49,4 +81,88 @@ public class Main {
             laptops.add(new Laptops(model, ramSize, hddSize, color, price));
         }
     }
+
+    // Создание фильтра
+    private static void filterLaptop(Set<Laptops> laptops) {
+        Map<String, Integer> dataFilter = new HashMap<>();
+        dataFilter.put("ram", 0);
+        dataFilter.put("hdd", 0);
+        dataFilter.put("price", 0);
+        boolean flag = true;
+        while (flag) {
+            System.out.printf("Параметры фильтра. Минимальный объем ОЗУ = %s Гб, " +
+                    "минмальный объем HDD = %s Гб, минимальная стоимость = %s руб.\n", dataFilter.get("ram"), dataFilter.get("hdd"), dataFilter.get("price"));
+            System.out.println("Пожалуйста, введите критерий, необходимый для фильтрации:");
+            System.out.println("1 - Объем ОЗУ");
+            System.out.println("2 - Емкость жесткого диска");
+            System.out.println("3 - Стоимость");
+            System.out.println("4 - Показать результат фильтрации");
+            Scanner scan = new Scanner(System.in);
+            String action = scan.next();
+            if (isNumber(action)) {
+                switch (Integer.parseInt(action)) {
+                    case 1:
+                        System.out.println("Введите минимальный объем ОЗУ ноутбука в Гб");
+                        String minRAM = scan.next();
+                        if (isNumber(minRAM) && Integer.parseInt(minRAM) > 0) {
+                            dataFilter.put("ram", Integer.parseInt(minRAM));
+                        } else {
+                            System.out.println("Ошибка ввода\n");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Введите минимальный объем жесткого диска (HDD) в Гб");
+                        String minHDD = scan.next();
+                        if (isNumber(minHDD) && Integer.parseInt(minHDD) > 0) {
+                            dataFilter.put("hdd", Integer.parseInt(minHDD));
+                        } else {
+                            System.out.println("Ошибка ввода\n");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Введите минимальную стоимость ноутбука в рублях");
+                        String minPrice = scan.next();
+                        if (isNumber(minPrice) && Integer.parseInt(minPrice) > 0) {
+                            dataFilter.put("price", Integer.parseInt(minPrice));
+                        } else {
+                            System.out.println("Ошибка ввода\n");
+                        }
+                        break;
+                    case 4:
+                        flag = !flag;
+                        showFilterResult(laptops, dataFilter);
+                        System.out.println();
+                        break;
+                    default:
+                        System.out.println("Некорректный ввод!");
+                        break;
+                }
+            } else {
+                System.out.println("Ошибка ввода");
+            }
+        }
+    }
+
+    // Вывод на экран результат фильтра
+    private static void showFilterResult(Set<Laptops> laptops, Map<String, Integer> dataFilter) {
+        for (Laptops laptop : laptops) {
+            if (laptop.hdd >= dataFilter.get("hdd") && laptop.ram >= dataFilter.get("ram") && laptop.price >= dataFilter.get("price")) {
+                System.out.println(laptop);
+            }
+        }
+    }
+
+
+    //    Проверка, является ли ввод с клавиатуры числом
+    private static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
 }
+
